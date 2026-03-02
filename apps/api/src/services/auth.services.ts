38cbe4a15126c0afdd977d6e1a6a87e50c844ca4
Nawaz Khan <abunawaz2905@gmail.com>
2026-03-03 00:24:35 +0530

    auth services add

---
+++ apps/api/src/services/auth.services.ts
@@ -0,0 +1,47 @@
+import {prisma} from "@repo/db/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {JWT_PASSCODE} from "@repo/backend-common/config"
+
+export async function signup(email: string, password:string){
+    const existing = await prisma.user.findUnique({
+        where: {email},
+    });
+
+    if(existing){
+        throw new Error("User alredy exist")
+    }
+
+    const hashed = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            password: hashed
+        }
+    });
+    return user
+
+}
+
+export async function signin(email: string , password: string){
+    const user = await prisma.user.findUnique({
+        where:{email},
+    });
+
+    if(!user){
+        throw new Error("invalid credentials");
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if(!passwordMatch){
+        throw new Error("Worng password!")
+    }
+    const token = jwt.sign(
+        {userId: user.id},
+        JWT_PASSCODE
+    )
+    return token
+
+}
