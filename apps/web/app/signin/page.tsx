2aa2b782e6e5dcab5369793cd964377fc499cab0
Nawaz Khan <abunawaz2905@gmail.com>
2026-04-01 01:40:01 +0530

    signup/signin and sidebar recreate

---
+++ apps/web/app/signin/page.tsx
@@ -0,0 +1,5 @@
+import { AuthScreen } from "../login/page";
+
+export default function SigninPage() {
+  return <AuthScreen initialMode="signin" />;
+}
---
+++ apps/web/app/signup/page.tsx
@@ -0,0 +1,5 @@
+import { AuthScreen } from "../login/page";
+
+export default function SignupPage() {
+  return <AuthScreen initialMode="signup" />;
+}
