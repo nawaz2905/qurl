6fdebcbd429bcfd7446ee7c124d47ac4a4c3c806
Nawaz Khan <abunawaz2905@gmail.com>
2026-04-05 21:26:40 +0530

    wrapped in suspense

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { AuthScreen } from "../login/page";
 
 export default function SignupPage() {
-  return <AuthScreen initialMode="signup" />;
+  return (
+    <Suspense fallback={<main className="min-h-screen bg-[#fff8f2]" />}>
+      <AuthScreen initialMode="signup" />
+    </Suspense>
+  );
 }
